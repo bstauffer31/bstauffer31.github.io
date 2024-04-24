250afeeae63e6ca421230e4c3635c4205da86a9a
bstauffer31 <165042499+bstauffer31@users.noreply.github.com>
2024-04-23 23:32:36 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+function menuClick(thissy, newshow, newshow2) {
 
+  thisid = thissy.id; 
+
+  avar='main'
+  bvar='show'
+  cvar='main show'
+  alldivs = document.getElementsByClassName('main show')
+
+  for (let data of alldivs) {
+    console.log(data.id)
+    document.getElementById(data.id).className="main noshow";
+    }
+
+    document.getElementById(newshow).className="main show";
+
+}
